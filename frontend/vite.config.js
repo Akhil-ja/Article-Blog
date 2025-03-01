2153d259790ff7f5851a519788f253b7abc781ef
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-03-01 22:30:17 +0530

    chore(frontend): dependencies and dev configs added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8001,
+    proxy: { "/api": { target: "http://localhost:5000" } },
+    changeOrigin: true,
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
