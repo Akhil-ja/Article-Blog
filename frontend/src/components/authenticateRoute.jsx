8a6a4d280addc8cd30c43a3ff115ef0114b026c8
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-03-01 22:43:58 +0530

    feat(frontend):authenticateRoute component added

---
+++ frontend/src/components/authenticateRoute.jsx
@@ -0,0 +1,35 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+export const AuthenticatedRoute = () => {
+  const location = useLocation();
+  const authInfo = sessionStorage.getItem("authInfo");
+  const publicOnlyRoutes = [
+    "/register",
+    "/login",
+    "/verify-otp",
+    "/forgot-password",
+    "/reset-password",
+    "/verify-otp",
+  ];
+
+  let isAuthenticated = false;
+
+  try {
+    isAuthenticated = authInfo ? JSON.parse(authInfo) : false;
+  } catch (error) {
+    console.error("Error parsing auth info:", error);
+    sessionStorage.removeItem("authInfo");
+  }
+
+  if (isAuthenticated && publicOnlyRoutes.includes(location.pathname)) {
+    return <Navigate to="/user/home" replace />;
+  }
+
+  if (!isAuthenticated && !publicOnlyRoutes.includes(location.pathname)) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthenticatedRoute;
