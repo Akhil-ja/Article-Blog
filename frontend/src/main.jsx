11df9ec0a7b146bd2967df3790d32e94731c088f
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-03-01 22:35:08 +0530

    fix(frontend):changed App amd  main file,defined routes ans css file edited

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AuthenticatedRoute from "./components/authenticateRoute";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { LoginPage } from "./screens/LoginPage";
+import { RegisterPage } from "./screens/RegisterPage";
+import HomePage from "./screens/homePage";
+import { OTPVerificationPage } from "./screens/OTPVerificationPage";
+import ForgotPasswordPage from "./screens/ForgotPasswordPage";
+import ResetPasswordPage from "./screens/ResetPasswordPage";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Navigate to="/login" />} />{" "}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/verify-otp" element={<OTPVerificationPage />} />
+      <Route element={<AuthenticatedRoute />}>
+        <Route path="/user/home" element={<HomePage />} />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
