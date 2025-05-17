93b5eb86184db31021a280208da48a90fdfa2ee8
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-05-17 11:47:30 +0530

    feat(Frontend,types): add user types

--- frontend/src/Types/userType.ts
+++ frontend/src/Types/userType.ts
@@ -11,3 +11,43 @@ export interface AuthState {
   error: any;
   success: string | null;
 }
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  preferences: string[];
+  isVerified: boolean;
+  createdAt?: string;
+}
+
+export interface UserProfile {
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  dob?: string;
+}
+
+export interface UserPreferences {
+  preferences: string[];
+}
+
+export type FeedbackType = "like" | "dislike";
+
+export interface Feedback {
+  userId: string;
+  articleId: string;
+  type: FeedbackType;
+  createdAt: string;
+}
+
+export interface UserState {
+  profile: null | User;
+  preferences: string[];
+  categories: string[];
+  activities: [];
+  loading: boolean;
+  error: null | any;
+  success: null | string;
+}
